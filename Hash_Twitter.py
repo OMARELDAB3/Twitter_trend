4349a6204dfc09e8e834d47f9dec4b9d7bf8e4be
Omarnemo <omarahmed0119049@gmail.com>
2022-07-18 02:53:47 +0200

    Update Hash_Twitter.py

--- Hash_Twitter.py
+++ Hash_Twitter.py
@@ -1,7 +1,8 @@
 red = '\033[31m'
 green = '\033[1;32m'
 white = '\033[1;38m'
-print(f"""{green}قُل لَّن يُصِيبَنَآ إِلَّا مَا كَتَبَ ٱللَّهُ لَنَا هُوَ مَوْلَىٰنَا ۚ وَعَلَى ٱللَّهِ فَلْيَتَوَكَّلِ ٱلْمُؤْمِنُونَ
+print(f"""قُل لَّن يُصِيبَنَآ إِلَّا مَا كَتَبَ ٱللَّهُ لَنَا
+
 
 {white}This script is closed forever
 Script developer is no longer available
