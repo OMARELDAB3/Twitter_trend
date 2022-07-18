0ba6a37b4ba61bd35a5a973d43d43d6bb795c6d2
Omarnemo <omarahmed0119049@gmail.com>
2022-07-18 02:35:15 +0200

    Create Hash_Twitter.py

---
+++ Hash_Twitter.py
@@ -0,0 +1,20 @@
+red = '\033[31m'
+green = '\033[1;32m'
+white = '\033[1;38m'
+print(f"""
+This script is closed forever
+Script developer is no longer available
+You are in a country where you cannot live
+You just have to hide to live
+Do not try to search for your rights as a citizen of the state of Egypt
+They will make your life dark
+
+{red}Mohamed Anis has disappeared
+ADMINS Groub the [Internet community in Egypt 2] disappears
+Now it's ELDab3's turn to disappear{white}
+
+The reason is one
+
+Don't listen to someone telling you to {red}#KEEPGOING{white}
+
+There is always darkness""")
